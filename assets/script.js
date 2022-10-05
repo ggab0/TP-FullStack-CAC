e49b655eaf2da8ed50afdaa4ddb2014013b093dc
gabo <gabriel_gonzalezmendez@hotmail.com>
2022-10-05 12:13:57 -0300

    solución de problemas y cambios al side

--- assets/script.js
+++ assets/script.js
@@ -2,37 +2,19 @@
 document.getElementById("btn_open").addEventListener("click", open_close_menu);
 
 //Declaramos variables
-var menu_side = document.getElementById("menu__side");
+var side_menu = document.getElementById("menu_side");
 var btn_open = document.getElementById("btn_open");
 var body = document.getElementById("body");
 
 //Evento para mostrar y ocultar menú
-    function open_close_menu(){
-        body.classList.toggle("body_move");
-        menu_side.classList.toggle("menu__side_move");
-    }
+function open_close_menu(){
+    body.classList.toggle("body_move");
+    side_menu.classList.toggle("menu__side_move");
+}
 
 //Si el ancho de la página es menor a 760px, ocultará el menú al recargar la página
 
-if (window.innerWidth < 760){
+if (window.innerWidth > 760){
     body.classList.toggle("body");
     menu_side.classList.toggle("menu_side");
 }
-
-//Haciendo el menú responsive(adaptable)
-
-// window.addEventListener("click", function(){
-
-//     if (window.innerWidth > 760){
-
-//         body.classList.remove("body_move");
-//         side_menu.classList.remove("menu__side_move");
-//     }
-
-//     if (window.innerWidth < 760){
-
-//         body.classList.add("body_move");
-//         side_menu.classList.add("menu__side_move");
-//     }
-
-// });
