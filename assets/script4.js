0bd7b376e03325d0b7dc76c3b39012010d4b6840
ggab0 <gabriel_gonzalezmendez@hotmail.com>
2022-10-18 20:08:13 -0300

    nuevos cambios

--- assets/script4.js
+++ assets/script4.js
@@ -1,9 +1,7 @@
-function myFunction() {
-    const element = document.getElementById("zona1");
-    let x = element.scrollLeft;
-    let y = element.scrollTop;
-    document.getElementById ("demo").innerHTML = "Horizontally: " + x.toFixed() + "<br>Vertically: " + y.toFixed();
-  }
+window.addEventListener("scroll", function() {
+  var header = document.querySelector("header");
+  header.classList.toggle("abajo", window.scrollY > 0);
+})
     // var header = document.querySelector("header");
     //  var zona1 = document.querySelector("section")
     // header.classList.toggle("abajo", zona1.scrollTop > 0);
